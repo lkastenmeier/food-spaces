d4955fc6db8e7b9a5cb9d259066085d55a50337b
Lena Kastenmeier <l.kastenmeier@web.de>
2019-09-13 09:24:14 +0200

    add autogenerated cardcontent for restaurants with styling

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,7 +1,7 @@
 import React from "react";
 function Restaurant(props) {
   return (
-    <li className="Restaurant">
+    <article className="Restaurant">
       <div className="ImgContainer">
         <img
           className="RestaurantImage"
@@ -17,9 +17,30 @@ function Restaurant(props) {
       </div>
       <div className="RestaurantInfo">
         <h2 className="RestaurantName">{props.restaurant.title}</h2>
-        <p className="RestaurantDescription">Lecker Lecker Lecker</p>
+
+        <div className="RestaurantCategories">
+          {props.restaurant.categories.map(element => {
+            return (
+              <p className="RestaurantCategory" key={props.restaurant.title}>
+                {element}
+              </p>
+            );
+          })}
+        </div>
+        <div className="RestaurantInfo">
+          <p className="RestaurantDescription" key={props.restaurant.title}>
+            {props.restaurant.description}
+          </p>
+          <p className="RestaurantDescription" key={props.restaurant.title}>
+            Distance: {props.restaurant.distance} minute(s)
+          </p>
+          <p className="RestaurantDescription" key={props.restaurant.title}>
+            Rating:
+            {props.restaurant.rating} stars
+          </p>
+        </div>
       </div>
-    </li>
+    </article>
   );
 }
 export default Restaurant;
